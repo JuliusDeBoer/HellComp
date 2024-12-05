251851348a5993bcfa132ba8a80b4e6b33c1536a
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2024-12-05 09:41:54 +0100

    feat: Add nix flake build

--- flake.nix
+++ flake.nix
@@ -13,7 +13,25 @@
       inherit system;
     };
   in {
-    devShell.x86_64-linux = pkgs.mkShell {
+    packages.${system}.default = pkgs.stdenv.mkDerivation {
+      name = "hellcomp";
+      src = ./.;
+
+      nativeBuildInputs = with pkgs; [
+        zig
+      ];
+
+      buildPhase = ''
+          mkdir -p $out/cache $out/global-cache
+          zig build --release=fast --cache-dir $out/cache --global-cache-dir $out/global-cache
+      '';
+
+      installPhase = ''
+        mkdir -p $out/bin
+        cp zig-out/bin/* $out/bin/
+      '';
+    };
+    devShell.${system} = pkgs.mkShell {
       buildInputs = with pkgs; [
         zig
         zls
