239e34279dcfe61e6d8411e7dd67a623e97b7bb6
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2025-10-08 19:53:06 +0200

    refactor: Use flake-parts

--- flake.nix
+++ flake.nix
@@ -1,42 +1,45 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell.url = "github:numtide/devshell";
   };
 
-  outputs = {
-    self,
-    nixpkgs
-  }: let
-    system = "x86_64-linux";
+  outputs = inputs@{ flake-parts, ...  }: flake-parts.lib.mkFlake { inherit inputs; } (top@{ config, withSystem, moduleWithSystem, ... }: {
+    systems = [
+      "x86_64-linux"
+    ];
+    imports = [
+      inputs.devshell.flakeModule
+    ];
+    perSystem = { config, pkgs, ... }: {
+      devshells.default = {
+        packages = with pkgs; [
+          zig
+          zls
+        ];
+      };
 
-    pkgs = import nixpkgs {
-      inherit system;
+      packages.default = pkgs.stdenv.mkDerivation {
+        name = "hellcomp";
+        src = ./.;
+
+        nativeBuildInputs = with pkgs; [
+          zig
+        ];
+
+        buildPhase = ''
+            mkdir -p $out/cache $out/global-cache
+            zig build --release=fast --cache-dir $out/cache --global-cache-dir $out/global-cache
+            rm -rf $out/cache $out/global-cache
+        '';
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cp zig-out/bin/* $out/bin/
+        '';
+      };
     };
-  in {
-    packages.${system}.default = pkgs.stdenv.mkDerivation {
-      name = "hellcomp";
-      src = ./.;
-
-      nativeBuildInputs = with pkgs; [
-        zig
-      ];
-
-      buildPhase = ''
-          mkdir -p $out/cache $out/global-cache
-          zig build --release=fast --cache-dir $out/cache --global-cache-dir $out/global-cache
-          rm -rf $out/cache $out/global-cache
-      '';
-
-      installPhase = ''
-        mkdir -p $out/bin
-        cp zig-out/bin/* $out/bin/
-      '';
-    };
-    devShell.${system} = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        zig
-        zls
-      ];
-    };
-  };
+  }
+);
 }
